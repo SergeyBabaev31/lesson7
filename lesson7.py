bc0b90d10664b64e75b696924695502e4c5966d6
Sergey Babaev <Babaew.serzh2015@yandex.ru>
2020-07-10 14:50:29 +0300

    Изменения

--- lesson7.py
+++ lesson7.py
@@ -1,89 +1,62 @@
-def def_cook_book():
-    with open('recipes.txt', encoding='utf-8-sig') as f:
-        cook_book = {}
-        while True:
-            cook = f.readline().strip()
-            cook_book[cook] = []
-            count = f.readline().strip()
-            i = 0
-            while i < int(count):
-                a = f.readline().strip().split(' | ')
-                cook_book[cook].append({'ingredient_name': a[0], 'quantity': a[1],'measure': a[2]})
-                i += 1
-            if f.readline() == '\n':
-                continue
-            else:
-                break
+from pprint import pprint
+
+
+# Задача №1
+def create_cook_book(File_name):
+    cook_book = {}
+
+    try:
+
+        with open('recipes.txt', encoding='utf-8') as f:
+            lst = [line.strip() for line in f]
+
+
+        for i, c in enumerate(lst):
+            if c.isdigit():
+                cook_book[lst[i-1]] = []
+
+
+                for slice in lst[i+1:i+int(c)+1]:
+                    ingredient_name = slice.split('|')[0]
+                    quantity = int(slice.split('|')[1])
+                    measure = slice.split('|')[2]
+
+                    cook_book[lst[i-1]].append({'ingredient_name':ingredient_name,'quantity':quantity,'measure':measure})
         return cook_book
 
+    except FileNotFoundError:
+        return(f'Файл: {file_name} не найден.')
+    except Exception as error:
+        return f'Ошибка - {error}'
 
 
-def get_shop_list_by_dishes(dishes, person_count):
-    lc_dict2buy = {}
-    list_ing4dish = {}
-    loc_dict_cook_book = read_cook_book()
-    for dish in dishes:
-        if (dish == '' or dish == ' '):
-            continue
-        list_ing4dish = loc_dict_cook_book.get(str(dish))   #получен списко ингредиентов для блюда
-        for ing in list_ing4dish:
-            if ing['ingridient_name'] not in lc_dict2buy.keys():
-                lc_dict2buy[ing['ingridient_name']] = {'measure': ing['measure'], 'quantity': 0}
-            lc_dict2buy[ing['ingridient_name']]['quantity'] = lc_dict2buy[ing['ingridient_name']]['quantity'] + ( int(ing['quantity'] )) * person_count
-    return lc_dict2buy
+# Задача №2
+
+def get_shop_list_by_dishes(dishes, cooking_book, person_count):
+    ing_dict = {}
+
+    for key in cooking_book.keys ():
+        for dish in dishes:
+            if key == dish:
+                for dictionary in cooking_book[key]:
+                    ing_name = dictionary['ingredient_name']
+
+                    try:
+                        ing_dict[ing_name]['quantity'] += (dictionary['quantity'] * person_count)
+                    except:
+                        ing_dict[ing_name] = {'measure': dictionary['measure'],'quantity': dictionary['quantity'] * person_count}
+
+    return ing_dict
 
 
+#####################################
 
-def get_dishes():
-    loc_list = []
-    loc_list.append('Запеченный картофель')
-    loc_list.append('Омлет')
-    return loc_list
-
-
-def get_persons_num():
-    return 2
+# Задача №1
+print('Задача №1:\n')
+pprint(create_cook_book('recipes.txt'))
+print('\n' * 3)
 
 
-def read_cook_book():
-    loc_dict = {}
-    lv_dish_name = ''
-    lv_ing_num = 0
-    lv_ing_proc = 0
-    lv_ingredient_line = ''
-
-    with open('recipes.txt', 'rt', encoding='UTF8') as cook_book_file:
-        for line in cook_book_file:
-            lv_dish_name = line.strip()
-            list_ing4dish = []
-            lv_ing_num = cook_book_file.readline().strip()  # название блюда
-            lv_ing_proc = 0
-            while (int(lv_ing_num) > lv_ing_proc):
-                dict_ing_info = {}
-                list_ing_info = []
-                lv_ingredient_line = cook_book_file.readline().strip()  # строка с одним ингеридентом
-                list_ing_info = lv_ingredient_line.split('|')
-                dict_ing_info['ingridient_name'] = list_ing_info[0]
-                dict_ing_info['quantity'] = list_ing_info[1]
-                dict_ing_info['measure'] = list_ing_info[2]
-                list_ing4dish.append(dict_ing_info)
-                lv_ing_proc += 1
-            loc_dict[str(lv_dish_name)] = list_ing4dish
-            cook_book_file.readline()  # пустая строка - просто перещелкиваем
-    return loc_dict
-
-def read_cook_book_anc_calc_list():
-    loc_dict_cook_book = read_cook_book()
-    print(loc_dict_cook_book)
-
-    print('Получаем количество блюд и персон')
-    loc_list_dishes = get_dishes()
-    loc_persons_num = get_persons_num()
-
-    print('Формируем список ингридиентов')
-    loc_list2buy = get_shop_list_by_dishes(loc_list_dishes, loc_persons_num)
-    print(loc_list2buy)
-
-print('Начало работы программы********************')
-read_cook_book_anc_calc_list()
-print('Завершение работы программы********************')
+# Задача №2
+print('Задача №2:\n')
+pprint(get_shop_list_by_dishes(['Омлет', 'Омлет'], create_cook_book('recipes.txt'), 2))
